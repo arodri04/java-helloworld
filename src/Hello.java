241b210f2660fc62e1c57880a1e80d7b54954a7d
Sam Rodriguez <augustine.rodriguez55@gmail.com>
2020-07-06 18:40:35 -0700

    more fun with println

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,10 @@ public class Hello {
 
     public static void  main(String[] args) {
         System.out.println("Hello, Sam!");
+
+        int FirstNumber = 5; // lets just add a variable
+        int SecondNumber = 10 + (FirstNumber * FirstNumber);
+
+        System.out.println("The First number is: " + FirstNumber + " The second number is: " + SecondNumber );
     }
 }
